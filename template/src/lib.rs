2bfd16f01389b6cf18c71f066c8765ff28964c23
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-11-04 15:15:10 -0700

    Counter

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,11 +1,38 @@
 use wasm_bindgen::JsValue;
-use wasm_react::{export_components, h, Component, VNode};
+use wasm_react::{export_components, h, hooks::use_state, Callback, Component, VNode};
+
+struct Counter {
+    initial_counter: i32,
+}
+
+impl Component for Counter {
+    fn render(&self) -> VNode {
+        let counter = use_state(|| self.initial_counter);
+
+        let vnode = h!(div).build((
+            h!(p).build(("Counter: ", *counter.value())),
+            h!(button)
+                .on_click(&Callback::new({
+                    let mut counter = counter.clone();
+                    move |_| counter.set(|c| c + 1)
+                }))
+                .build("+1"),
+            h!(button)
+                .on_click(&Callback::new({
+                    let mut counter = counter.clone();
+                    move |_| counter.set(|c| c - 1)
+                }))
+                .build("-1"),
+        ));
+        vnode
+    }
+}
 
 pub struct App;
 
 impl Component for App {
     fn render(&self) -> VNode {
-        h!(div).build("hi")
+        Counter { initial_counter: 0 }.build()
     }
 }
 
