2da08c469ce6e1651e89c55c4e78773d14bf8b5b
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-02-17 14:05:12 -0800

    Use workbox for automatic offline availability

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,17 +1,22 @@
 const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const dist = path.resolve(__dirname, "dist");
 
-module.exports = {
+module.exports = env => ({
   mode: "development",
   experiments: {
     asyncWebAssembly: true,
   },
   entry: {
-    index: "./pkg/index.js",
+    index: [
+      "./pkg/index.js",
+      ...env.WEBPACK_BUILD
+        ? ["./js/register-sw.js"]
+        : []
+    ],
   },
   output: {
     path: dist,
@@ -27,5 +32,16 @@ module.exports = {
     new WasmPackPlugin({
       crateDirectory: __dirname,
     }),
+    ...env.WEBPACK_BUILD
+      ? [
+        new WorkboxPlugin.GenerateSW({
+          // these options encourage the ServiceWorkers to get in there fast
+          // and not allow any straggling "old" SWs to hang around
+          clientsClaim: true,
+          skipWaiting: true,
+          swDest: 'service-worker.js'
+        })
+      ]
+      : [],
   ],
-};
+});
